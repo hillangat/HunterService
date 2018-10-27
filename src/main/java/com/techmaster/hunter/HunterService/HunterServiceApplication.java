baec7f9ca9abe9c03cd6b57c26402b879a100b44
Kip <hillangat@gmail.com>
2018-10-27 15:28:16 -0400

    Initial commit

---
+++ src/main/java/com/techmaster/hunter/HunterService/HunterServiceApplication.java
@@ -0,0 +1,12 @@
+package com.techmaster.hunter.HunterService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HunterServiceApplication.class, args);
+	}
+}
